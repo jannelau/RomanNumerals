58a8dd37796e54e74b34117206f652762b26d2ac
Järjestelmänvalvoja <Jrjestelmnvalvoja@10.20.194.4>
2016-10-13 12:58:41 +0300

    aaa

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
 
-public class RomanNumerals {
-	public int convertToInteger(String romanNum) {
+
+public class RomanNumerals {;
+	
+	public int convertToIntegerStupid(String romanNum) {
 		int number = 0;
 		if (romanNum == "I"){
 			number = 1;
@@ -8,8 +11,19 @@ public class RomanNumerals {
 			number = 2;
 		}
 		
-		
 		return number;
 		
+	}
+	
+	public int convertToInteger(String romanNum) {
+		int number = 0;
+		char c;
+		int i;
+		
+		for (i = 0; i <romanNum.length(); i++) {
+
+		}
+		
+		return number;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -7,16 +7,22 @@ public class TestRomanNumerals {
 
 	@Test
 	public void test_I_is_1() {
-		int number = rom.convertToInteger("I");
+		int number = rom.convertToIntegerStupid("I");
 		
 		
 		assertEquals (1, number);
 	}
 	public void test_II_is_2() {
-		int number = rom.convertToInteger("II");
+		int number = rom.convertToIntegerStupid("II");
 		
 		
 		assertEquals (2, number);
 	}
 
+	public void test_I_is_1b() {
+		int number = rom.convertToInteger("I");
+		
+		
+		assertEquals (1, number);
+	}
 }
